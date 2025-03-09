fe00f3062ea6a4e21145297c4c6c5d9bf6902556
techno-user314 <zharwood314@gmail.com>
2025-03-09 17:12:59 -0600

    Create settings.py

---
+++ source/settings.py
@@ -0,0 +1,10 @@
+# What year to generate information for
+YEAR = 2025
+
+# Lat, long coords. What location to get sunrise/set times for
+SUN_LOC = [60.48, -161.46]
+
+# NOAA station ID's that determine where tides are pulled from
+LOCATIONS = [9466477, #NOAA station ID for Bethel, AK,
+             9465831, #NOAA station ID for Quinhagak, AK,
+             8467373] #NOAA station ID for Togaik, AK
