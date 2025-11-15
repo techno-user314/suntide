26b2c54ae05076ec49f30ea3dae049cacda09237
techno-user314 <zharwood314@gmail.com>
2025-11-14 18:51:20 -0700

    Create getSunTideData function

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+import "./sun.js";
+import { getNextTides } from "./tides.js";
+
+export async function getSunTideData(date, timeOfDay) {
+  const sunrise = date.sunrise();
+  const sunset = date.sunset();
+
+  // --- Suntime data ---
+  const daylightDuration = sunset - sunrise;
+
+  const yesterday = new Date(date);
+  yesterday.setDate(date.getDate() - 1);
+
+  const yesterdaySunrise = yesterday.sunrise();
+  const yesterdaySunset = yesterday.sunset();
+  const yesterdayDuration = yesterdaySunset - yesterdaySunrise;
+
+  const daylightDifference = daylightDuration - yesterdayDuration;
+
+  // --- Tides for all station IDs ---
+  const stationIds = {9466477, 9465831, 8467373};
+  const tidesByStation = {};
+  for (const id of stationIds) {
+    tidesByStation[id] = await getNextTides(id, date, timeOfDay);
+  }
+
+  return {
+    sunrise,
+    sunset,
+    daylightDuration,
+    daylightDifference,
+    tides: tidesByStation
+  };
+}
